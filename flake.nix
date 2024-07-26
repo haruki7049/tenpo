f1676fd9dabc5e6fd4052223dab316b9884af79a
haruki7049 <64677724+haruki7049@users.noreply.github.com>
2024-07-26 18:19:50 +0900

    ci: Setup cargo-llvm-cov (#1)
    * ci: WIP
    
    * build: Init llvm-cov
    
    * ci: Init build-cargo-llvm-cov
    
    * chore: Rename cov to llvm-cov
    
    * ci: hoge
    
    * ci: Init build-cargo-llvm-cov-text
    
    * ci: Add llvm-cov in checks
    
    * ci: Update flake.nix
    
    * ci: Update nix-checker

--- flake.nix
+++ flake.nix
@@ -58,19 +58,37 @@
           libxkbcommon
           wayland
         ];
+        llvm-cov-text = craneLib.cargoLlvmCov {
+          inherit cargoArtifacts src;
+          buildInputs = bevyengine-dependencies;
+          nativeBuildInputs = [ pkgs.pkg-config ];
+          cargoExtraArgs = "--locked";
+          cargoLlvmCovCommand = "test";
+          cargoLlvmCovExtraArgs = "--text --output-dir $out";
+        };
+        llvm-cov = craneLib.cargoLlvmCov {
+          inherit cargoArtifacts src;
+          buildInputs = bevyengine-dependencies;
+          nativeBuildInputs = [ pkgs.pkg-config ];
+          cargoExtraArgs = "--locked";
+          cargoLlvmCovCommand = "test";
+          cargoLlvmCovExtraArgs = "--html --output-dir $out";
+        };
       in
       {
         formatter = treefmtEval.config.build.wrapper;
 
         packages.default = kosu;
         packages.doc = cargo-doc;
+        packages.llvm-cov = llvm-cov;
+        packages.llvm-cov-text = llvm-cov-text;
 
         apps.default = flake-utils.lib.mkApp {
           drv = self.packages.${system}.default;
         };
 
         checks = {
-          inherit kosu cargo-clippy cargo-doc;
+          inherit kosu cargo-clippy cargo-doc llvm-cov llvm-cov-text;
           formatting = treefmtEval.config.build.check self;
         };
 
