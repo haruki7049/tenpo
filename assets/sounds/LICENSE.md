addcf94258c00a2113eb467ecad7cc43a635989b
haruki7049 <tontonkirikiri@gmail.com>
2025-10-25 19:02:48 +0900

    Update

---
+++ assets/sounds/LICENSE.md
@@ -0,0 +1,6 @@
+# Credits
+
+Sound Effects: MaouDamashii
+
+- ./maou_se_8bit02.ogg
+- ./maou_se_8bit16.ogg
--- src/main.rs
+++ src/main.rs
@@ -7,45 +7,36 @@ fn main() {
 
     App::new()
         .add_plugins(DefaultPlugins)
-        .add_systems(Startup, tenpo::setup)
-        .add_systems(Update, tenpo::sprite_movement)
-        .add_systems(Update, bevy::input::keyboard::keyboard_input_system)
-        .add_systems(Update, update_system)
+        .add_systems(Startup, setup_audio)
+        .add_systems(Update, play_sound_on_timer)
         .run();
 }
 
-fn update_system(keyboard: Res<ButtonInput<KeyCode>>) {
-    for key in keyboard.get_just_pressed() {
-        match key {
-            KeyCode::KeyA
-            | KeyCode::KeyB
-            | KeyCode::KeyC
-            | KeyCode::KeyD
-            | KeyCode::KeyE
-            | KeyCode::KeyF
-            | KeyCode::KeyG
-            | KeyCode::KeyH
-            | KeyCode::KeyI
-            | KeyCode::KeyJ
-            | KeyCode::KeyK
-            | KeyCode::KeyL
-            | KeyCode::KeyM
-            | KeyCode::KeyN
-            | KeyCode::KeyO
-            | KeyCode::KeyP
-            | KeyCode::KeyQ
-            | KeyCode::KeyR
-            | KeyCode::KeyS
-            | KeyCode::KeyT
-            | KeyCode::KeyU
-            | KeyCode::KeyV
-            | KeyCode::KeyW
-            | KeyCode::KeyX
-            | KeyCode::KeyY
-            | KeyCode::KeyZ => {
-                println!("{:?} Pressed", key)
-            }
-            _ => (),
-        }
+// サウンド再生間隔を管理するタイマー
+#[derive(Resource)]
+struct SoundTimer(Timer);
+
+// 起動時にアセットをロードし、タイマーをリソースとして登録
+fn setup_audio(mut commands: Commands) {
+    // 1秒ごとに繰り返すタイマーを設定
+    commands.insert_resource(SoundTimer(Timer::from_seconds(1.0, TimerMode::Repeating)));
+}
+
+// タイマーを更新し、指定時間が経過したらサウンドを再生
+fn play_sound_on_timer(
+    mut commands: Commands,
+    time: Res<Time>, // 経過時間を取得
+    mut timer: ResMut<SoundTimer>, // タイマーリソース（可変）
+    asset_server: Res<AssetServer>,
+) {
+    // 毎フレーム、タイマーを更新
+    timer.0.tick(time.delta());
+
+    // タイマーがちょうど完了したか（1秒経過したか）をチェック
+    if timer.0.just_finished() {
+        // サウンドを再生
+        commands.spawn(AudioPlayer::new(
+            asset_server.load("sounds/maou_se_8bit02.ogg"),
+        ));
     }
 }
